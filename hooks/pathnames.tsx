b59feb493eccc5d7415dd87d94e79c62ef1a4a5c
Brutus <brutussegun@gmail.com>
2022-09-15 15:08:42 -0400

    Next up and running

---
+++ hooks/pathnames.tsx
@@ -0,0 +1,14 @@
+const pathnames = {
+  home: "/",
+  register: "/register",
+  login: "/authentication/login",
+  logout: "/logout",
+  dashboard: "/dashboard",
+  clients: "/client/clients",
+  projects: "/client/projects",
+  invoices: "/clients/projects/invoices",
+  history: "/clients/history",
+  profile: "/dashboard/profile",
+};
+
+export { pathnames };
